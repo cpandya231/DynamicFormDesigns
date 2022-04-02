e05fcfd3a93a7f42981a1195560aa14fa2684ca2
Vihit Shah <vihitshah@Vihits-MacBook-Air.local>
2022-04-02 18:06:50 +0530

    add db script

---
+++ scripts/db/db_user_roles_setup.sql
@@ -0,0 +1,34 @@
+create database logever;
+
+create table user (
+	id INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255),
+	password VARCHAR(255) NOT NULL,
+	create_dt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	update_dt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	CONSTRAINT UC_USER UNIQUE (username,email)
+);
+
+create table role (
+	id INT NOT NULL AUTO_INCREMENT,
+	role VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT UC_ROLE UNIQUE (role)
+)
+
+
+create table user_role (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	role_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (role_id) REFERENCES role(id)
+)
+
+create user 'logever'@'%' identified by 'logever'; -- Creates the user
+grant all on logever.* to 'logever'@'%'; --grant all access to user
