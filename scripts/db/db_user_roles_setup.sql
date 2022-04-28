41fd7d59a28a20cfcf608deb0a372c4dd8bfa60e
cpandya231 <theprime23@gmail.com>
2022-04-28 13:50:53 +0530

    Set some config

--- scripts/db/db_user_roles_setup.sql
+++ scripts/db/db_user_roles_setup.sql
@@ -1,4 +1,7 @@
+set global log_bin_trust_function_creators=1;
+
 create database if not exists logever;
 
 create user if not exists 'logever'@'%' identified by 'logever'; 
 grant all on logever.* to 'logever'@'%';
+
