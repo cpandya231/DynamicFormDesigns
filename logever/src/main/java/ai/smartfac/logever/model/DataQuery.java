4a3a8df833ec7ea41948fa1d63fa3c78f827bcde
Vihit Shah <vihitshah@Vihits-MacBook-Air.local>
2022-06-24 20:39:42 +0530

    Get Data for forms for authorized users

---
+++ logever/src/main/java/ai/smartfac/logever/model/DataQuery.java
@@ -0,0 +1,33 @@
+package ai.smartfac.logever.model;
+
+import java.sql.ResultSet;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataQuery {
+
+    private Map<String,String> data = new HashMap<>();
+
+    public DataQuery(ResultSet resultSet, String[] columns) {
+        Arrays.stream(columns).forEach(column-> {
+            try {
+                this.data.put(column,resultSet.getString(column));
+            } catch (Exception ex) {
+                System.out.println("Exception occurred : "+ex.getMessage());
+            }
+        });
+    }
+    public DataQuery(Map<String, String> data) {
+        this.data = data;
+    }
+
+    public Map<String, String> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, String> data) {
+        this.data = data;
+    }
+}
--- logever/src/main/java/ai/smartfac/logever/service/FormDataService.java
+++ logever/src/main/java/ai/smartfac/logever/service/FormDataService.java
@@ -1,11 +1,18 @@
 package ai.smartfac.logever.service;
 
 import ai.smartfac.logever.entity.Form;
+import ai.smartfac.logever.entity.State;
+import ai.smartfac.logever.model.DataQuery;
+import ai.smartfac.logever.model.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class FormDataService {
@@ -19,5 +26,16 @@ public class FormDataService {
 
     public void update(Form form, Map<String,String> values) {
         jdbcTemplate.execute(form.makeUpdateStmt(values));
+    }
+
+    public List<DataQuery> getAllFor(Form form, List<State> states) {
+        Table table = new Table();
+        table.setName(form.getName());
+        String selectCols = "id,"+form.getColumns()+",log_create_dt, created_by,log_update_dt,updated_by";
+        String selectStmt = "SELECT "+selectCols+" from "+table.getName()+" WHERE state in ("+
+                String.join(",",states.stream().map(f->"'"+f.getName()+"'").collect(Collectors.toList())) +")";
+
+        return jdbcTemplate.query(selectStmt,
+                (resultSet,rowNum)-> new DataQuery(resultSet,selectCols.split(",")));
     }
 }
