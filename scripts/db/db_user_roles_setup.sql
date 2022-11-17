f1da43316307103a9c2a2531528f788c4cbcd7ab
Chintan Pandya <theprime23@gmail.com>
2022-11-17 18:00:06 +0530

    Script changes

--- scripts/db/db_user_roles_setup.sql
+++ scripts/db/db_user_roles_setup.sql
@@ -1,6 +1,6 @@
 create database if not exists logever;
 
 create user if not exists 'logever'@'%' identified by 'logever'; 
-GRANT SUPER ON *.* TO 'logever'@'%'; IDENTIFIED BY 'logever';
+GRANT SUPER ON *.* TO 'logever'@'%' IDENTIFIED BY 'Logever@123';
 grant all on logever.* to 'logever'@'%';
 
