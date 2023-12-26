0f6b0b06ef26260ac0f213d577495c90fa8f9295
Vihit Shah <vihitshah@192.168.1.4>
2023-12-26 14:32:56 +0530

    designee2 null check

--- logever/src/main/java/ai/smartfac/logever/controller/FormDataController.java
+++ logever/src/main/java/ai/smartfac/logever/controller/FormDataController.java
@@ -161,7 +161,7 @@ public class FormDataController {
                                     if(initiator.getDepartment().getDesignee1()!=null && !initiator.getDepartment().getDesignee1().isEmpty())
                                         pendingEntries.add(new PendingEntry(form.getId(),entry.getEntryId(),initiator.getDepartment().getDesignee1(),null,null
                                                 , initiator.getUsername(), userService.getUserByUsername(initiator.getDepartment().getDesignee1()).get().getDepartment().getHod()));
-                                    if(initiator.getDepartment().getDesignee1()!=null && !initiator.getDepartment().getDesignee2().isEmpty())
+                                    if(initiator.getDepartment().getDesignee2()!=null && !initiator.getDepartment().getDesignee2().isEmpty())
                                         pendingEntries.add(new PendingEntry(form.getId(),entry.getEntryId(),initiator.getDepartment().getDesignee2(),null,null
                                                 , initiator.getUsername(), userService.getUserByUsername(initiator.getDepartment().getDesignee2()).get().getDepartment().getHod()));
                                 }
@@ -171,7 +171,7 @@ public class FormDataController {
                                 if(initiator.getDepartment().getDesignee1()!=null && !initiator.getDepartment().getDesignee1().isEmpty())
                                     pendingEntries.add(new PendingEntry(form.getId(),entry.getEntryId(),initiator.getDepartment().getDesignee1(),null,null
                                             , initiator.getUsername(), userService.getUserByUsername(initiator.getDepartment().getDesignee1()).get().getDepartment().getHod()));
-                                if(initiator.getDepartment().getDesignee1()!=null && !initiator.getDepartment().getDesignee2().isEmpty())
+                                if(initiator.getDepartment().getDesignee2()!=null && !initiator.getDepartment().getDesignee2().isEmpty())
                                     pendingEntries.add(new PendingEntry(form.getId(),entry.getEntryId(),initiator.getDepartment().getDesignee2(),null,null
                                         , initiator.getUsername(), userService.getUserByUsername(initiator.getDepartment().getDesignee2()).get().getDepartment().getHod()));
                         }
